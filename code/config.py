534bbec9ebd345aaffd13189194738a795aa90c6
Ganesh Nikumbh <ganesh.nikumbh@nl.abnamro.com>
2022-03-29 10:19:15 +0200

    added kafka producer code

---
+++ code/config.py
@@ -0,0 +1,5 @@
+PROJECT_NAME: str = "cqrs-kafka-producer"
+KAFKA_URI: str = "http://104.47.167.221"
+KAFKA_PORT: str = 9021
+KAFKA_INSTANCE = KAFKA_URI + ":" + KAFKA_PORT
+DEBUG: bool = False
--- code/main.py
+++ code/main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import List  # ネストされたBodyを定義するために必要
 from starlette.middleware.cors import CORSMiddleware  # CORSを回避するために必要
 from db import session  # DBと接続するためのセッション
 from model import UserTable, User  # 今回使うモデルをインポート
 import requests
+import asyncio
+from aiokafka import AIOKafkaProducer
+from config import KAFKA_INSTANCE
+from config import PROJECT_NAME
+import json
 
-app = FastAPI()
+app = FastAPI(title=PROJECT_NAME)
+
+
+loop = asyncio.get_event_loop()
+aioproducer = AIOKafkaProducer(
+    loop=loop, client_id=PROJECT_NAME, bootstrap_servers=KAFKA_INSTANCE
+)
+
+
+@app.on_event("startup")
+async def startup_event():
+    await aioproducer.start()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await aioproducer.stop()
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -37,6 +59,20 @@ async def create_user(name: str, age: int):
     user.age = age
     session.add(user)
     session.commit()
+
+    topicname = 'user'
+    user_obj = {'name':name,'age':age}
+
+    try:
+
+        await aioproducer.send(topicname, json.dumps(user_obj).encode("ascii"))
+
+    except Exception as e:
+        print(e)
+        return 400
+    
+
+    return 200
 
 @app.put("/users")
 # users=[{"id": 1, "name": "一郎", "age": 16},{"id": 2, "name": "二郎", "age": 20}]
