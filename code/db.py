84d1b7028cd88435c9e578a544ca3b2de21087c3
Ganesh Nikumbh <ganesh.nikumbh@nl.abnamro.com>
2022-03-29 07:45:39 +0200

    removed scoped session with property

--- code/db.py
+++ code/db.py
@@ -25,6 +25,16 @@ ENGINE = create_engine(
     echo=True
 )
 
+# # Sessionの作成
+# session = scoped_session(
+#     # ORM実行時の設定。自動コミットするか、自動反映するか
+#     sessionmaker(
+#         autocommit=False,
+#         autoflush=True,
+#         bind=ENGINE
+#     )
+# )
+
 # Sessionの作成
 session = sessionmaker(
         autocommit=False,
@@ -32,7 +42,8 @@ session = sessionmaker(
         bind=ENGINE
     )
 
-# modelで使用する
-Base = declarative_base()
-# DB接続用のセッションクラス、インスタンスが作成されると接続する
-Base.query = session.query_property()
+
+# # modelで使用する
+# Base = declarative_base()
+# # DB接続用のセッションクラス、インスタンスが作成されると接続する
+# Base.query = session.query_property()
